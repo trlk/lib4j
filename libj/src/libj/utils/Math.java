5fb38150a5032bb46db0beba4d5afef83f2f8108
Taras Lyuklyanchuk <tlyuklyanchuk@integrity.com.ua>
2015-03-16 14:26:10 +0200

    added BigDecimal conversion functions

--- libj/src/libj/utils/Math.java
+++ libj/src/libj/utils/Math.java
@@ -1,5 +1,7 @@
 package libj.utils;
 
+import java.math.BigDecimal;
+
 public class Math {
 
 	public static int min(int arg0, int arg1) {
@@ -22,6 +24,23 @@ public class Math {
 
 	public static int mod(int arg0, int arg1) {
 		return arg0 % arg1;
+	}
+
+	public static float toFloat(BigDecimal bdAmount) {
+
+		return bdAmount.floatValue();
+	}
+
+	public static BigDecimal toBigDecimal(float amount, int scale) {
+
+		BigDecimal bdAmount = new BigDecimal(amount);
+
+		return bdAmount.setScale(scale, BigDecimal.ROUND_HALF_UP);
+	}
+
+	public static BigDecimal toBigDecimal(float amount) {
+
+		return toBigDecimal(amount, 2);
 	}
 
 }
